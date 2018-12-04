62cc1bd2c7834d9ca07b9962c1d40cbcba21b81d
qu4ku <qu4ku@hotmail.com>
2018-12-04 05:26:32 +0100

    day01

--- day01/day01.py
+++ day01/day01.py
@@ -3,10 +3,27 @@ day 01:
 https://adventofcode.com/2018/day/1
 """
 
-with open('day01-input.txt', 'r') as f:
-	input_list = [int(x) for x in f.readlines() if x != '\n']
+from itertools import cycle
 
-total = 0
-for x in input_list:
-	total += x
-print(total)
+# Frist part
+with open('day01-input.txt', 'r') as f:
+	data = [int(x) for x in f.readlines() if x != '\n']
+
+ans = 0
+for x in data:
+	ans += x
+print(ans)  # ans = 497
+
+
+# Second part
+current_frequency = 0
+frequencies = [current_frequency]
+ans = False
+for x in cycle(data):
+	current_frequency += x
+	if current_frequency not in frequencies:
+		frequencies.append(current_frequency)
+	else:
+		ans = current_frequency
+		break
+print(ans)  # ans =558
