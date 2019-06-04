7bb948eef21ef7a90a729c0e15b7aa6c11f36736
qu4ku <qu4ku@hotmail.com>
2019-06-04 21:15:18 +0200

    d08

--- day07/day07.py
+++ day07/day07.py
@@ -3,6 +3,8 @@ day 07:
 https://adventofcode.com/2018/day/7
 """
 
+# NOT FINISHED
+
 import re
 
 # Load data
---
+++ day08/day08.py
@@ -0,0 +1,12 @@
+"""
+day 07:
+https://adventofcode.com/2018/day/7
+"""
+
+# NOT FINISHED
+
+import re
+
+# Load data
+
+
